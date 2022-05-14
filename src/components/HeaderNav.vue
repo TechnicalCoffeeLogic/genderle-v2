003781ad2e6faec0149f841a563f85fc0a817735
TechnicalCoffeeLogic <trevorconvirs@gmail.com>
2022-05-13 21:29:55 -0500

    Added header component and removed vite generic files

---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+defineProps({
+    
+})
+
+</script>
+
+<template>
+    <header>
+        <ul class="nav">
+            <li>
+                <div>
+                    <h1 class="title display-1">GENDERLE</h1>
+                </div>
+            </li>
+            <li class="nav-item">
+                <div>
+                    <a type="button" class="nav-link active" data-bs-toggle="modal" data-bs-target="#helpModal"
+                        aria-current="page" href="#">
+                        <i class="bi bi-question-circle"></i>
+                    </a>
+                </div>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<style scoped>
+a {
+    color: #42b983;
+}
+
+header{
+    border-bottom: 1px solid rgb(58,58,60);
+}
+
+.title {
+    color: gainsboro;
+    font-size: 2.5rem;
+    font-weight: bold;
+    margin: .4rem 0 .4rem 0;
+    text-align: center;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 
+    'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+
+.nav {
+    align-items: center;
+    justify-content: center;
+}
+
+#help-icon {
+    color: gainsboro;
+    font-size: 1.5rem;
+    text-align: right;   
+    margin: .4rem 0 .4rem 0;
+}
+</style>
